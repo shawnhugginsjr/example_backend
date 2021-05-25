b1b69de0959c79c8f6ccd37b085eb6e918e3998e
Shawn Huggins <hugginsjrshawn@gmail.com>
2021-05-25 18:35:33 +0900

    Impliment the Recipe class

--- recipe.rb
+++ recipe.rb
@@ -1,4 +1,76 @@
 class Recipe
-    # YOUR CODE HERE
+    @@inprogress_recipe = nil
+    @@recipes = {}
 
-  end
+    def initialize(name, ingredients, method_steps)
+        @name = name
+        @ingredients = ingredients
+        @method_steps = method_steps
+    end
+
+    attr_reader :name, :ingredients, :method_steps
+
+    class << self
+        def clear
+            @@recipes.clear
+        end
+
+        def describe
+            yield
+        end
+
+        def for(recipe_name)
+            unless recipe_name.is_a?(String)
+                raise ArgumentError.new(
+                    'Expected recipe_name to be a string'
+                  )
+            end
+            recipes[recipe_name]
+        end
+
+        def valid?(recipe)
+            valid_name = recipe.name.is_a?(String) && !recipe.name.strip.empty?
+            valid_name && !recipe.ingredients.empty? && !recipe.method_steps.empty?
+        end
+
+        def inprogress_recipe
+            @@inprogress_recipe
+        end
+
+        def inprogress_recipe=(recipe)
+            @@inprogress_recipe = recipe
+        end
+
+        def add_recipe(recipe)
+            return false unless Recipe.valid?(recipe)
+            @@recipes[recipe.name] = recipe
+            true
+        end
+
+        def recipes
+            @@recipes
+        end
+    end
+end
+
+def recipe_method
+    yield
+end
+
+def ingredient(recipe_ingredient)
+    Recipe.inprogress_recipe[:ingredients].append(recipe_ingredient)
+end
+
+def step(recipe_step)
+    Recipe.inprogress_recipe[:method_steps].append(recipe_step)
+end
+
+def recipe(recipe_name)
+    inprogress_recipe = {name: '', ingredients:[], method_steps: []}
+    inprogress_recipe[:name] = recipe_name
+    Recipe.inprogress_recipe = inprogress_recipe
+    yield
+    new_recipe = Recipe.new(inprogress_recipe[:name],inprogress_recipe[:ingredients], inprogress_recipe[:method_steps] )
+    Recipe.add_recipe(new_recipe)
+    Recipe.inprogress_recipe = nil
+end
